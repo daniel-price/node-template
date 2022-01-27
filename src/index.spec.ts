dcd58ad90395f9db0dad5b97117b6d4a3cb890e8
Daniel Price <daniel.price@wonderbill.com>
2022-01-27 11:08:20 +0000

    add placeholder test

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { main } from "./index";
+
+describe("index", () => {
+  it("test", () => {
+    main();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log("hello world");
+export function main() {
+  console.log("hello world");
+}
